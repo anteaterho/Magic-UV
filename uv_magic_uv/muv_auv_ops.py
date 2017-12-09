326f0262bc8c4e218e32033bc3999d3fda708ba2
Nutti <nutti.metro@gmail.com>
2017-12-09 23:10:24 +0900

    temp

--- uv_magic_uv/muv_auv_ops.py
+++ uv_magic_uv/muv_auv_ops.py
@@ -31,6 +31,294 @@ import bmesh
 from mathutils import Vector
 from bpy.props import EnumProperty
 
+from pprint import pprint
+
+from . import muv_common
+
+
+
+
+def get_vert_loop(bm, uv_layer):
+    sel_faces = [f for f in bm.faces if f.select]
+
+    # get candidate loops
+    cand_loops = []
+    for f in sel_faces:
+        for l in f.loops:
+            if l[uv_layer].select:
+                cand_loops.append(l)
+
+    if len(cand_loops) == 0:
+        return
+
+    # first_vert = cand_loops[0].vert
+    # pairs = []
+    # parsed_verts = []
+
+    # def find_linked(v, ps, uv_layer, parsed):
+    #     parsed.append(v)
+    #     for l in v.link_loops:
+    #         ov = l.link_loop_next.vert
+    #         # if there is same pair, skip it
+    #         found = False
+    #         for p in ps:
+    #             if (v in p) and (ov in p):
+    #                 found = True
+    #
+    #         if l[uv_layer].select:
+    #             if l.link_loop_next[uv_layer].select:
+    #                 if not found:
+    #                     ps.append([l.vert, l.link_loop_next.vert])
+    #                 if not l.link_loop_next.vert in parsed:
+    #                     find_linked(l.link_loop_next.vert, ps, uv_layer, parsed)
+    #
+    #         ov = l.link_loop_prev.vert
+    #         # if there is same pair, skip it
+    #         found = False
+    #         for p in ps:
+    #             if (v in p) and (ov in p):
+    #                 found = True
+    #                 break
+    #
+    #         if l[uv_layer].select:
+    #             if l.link_loop_prev[uv_layer].select:
+    #                 if not found:
+    #                     ps.append([l.vert, l.link_loop_prev.vert])
+    #                 if not l.link_loop_prev.vert in parsed:
+    #                     find_linked(l.link_loop_prev.vert, ps, uv_layer, parsed)
+    #
+    # find_linked(first_vert, pairs, uv_layer, parsed_verts)
+
+
+
+    first_loop = cand_loops[0]
+    loop_pairs = []
+    parsed_loops = []
+    def get_loop_pairs(l, pairs, uv_layer, parsed):
+        parsed.append(l)
+        for ll in l.vert.link_loops:
+            # forward direction
+            lln = ll.link_loop_next
+            # if there is same pair, skip it
+            found = False
+            for p in pairs:
+                if (ll in p) and (lln in p):
+                    found = True
+                    break
+
+            if ll[uv_layer].select and lln[uv_layer].select:
+                if not found:
+                    pairs.append([ll, lln])
+                if not lln in parsed:
+                    get_loop_pairs(lln, pairs, uv_layer, parsed)
+
+            # backward direction
+            llp = ll.link_loop_prev
+            # if there is same pair, skip it
+            found = False
+            for p in pairs:
+                if (ll in p) and (llp in p):
+                    found = True
+                    break
+
+            if ll[uv_layer].select and llp[uv_layer].select:
+                if not found:
+                    pairs.append([ll, llp])
+                if not llp in parsed:
+                    get_loop_pairs(llp, pairs, uv_layer, parsed)
+
+    # sort pair by vert
+    # (v0, v1) - (v1, v2) - (v2, v3) ....
+    def sort_loop_pairs(pairs):
+        rest = pairs
+        sorted_pairs = [rest[0]]
+        rest.remove(rest[0])
+        # prepend
+        while True:
+            p1 = sorted_pairs[0]
+            for p2 in rest:
+                if p1[0].vert == p2[0].vert:
+                    sorted_pairs.insert(0, [p2[1], p2[0]])
+                    rest.remove(p2)
+                    break
+                elif p1[0].vert == p2[1].vert:
+                    sorted_pairs.insert(0, [p2[0], p2[1]])
+                    rest.remove(p2)
+                    break
+            else:
+                break
+
+        # append
+        while True:
+            p1 = sorted_pairs[-1]
+            for p2 in rest:
+                if p1[1].vert == p2[0].vert:
+                    sorted_pairs.append([p2[0], p2[1]])
+                    rest.remove(p2)
+                    break
+                elif p1[1].vert == p2[1].vert:
+                    sorted_pairs.append([p2[1], p2[0]])
+                    rest.remove(p2)
+                    break
+            else:
+                break
+
+        return sorted_pairs
+
+    # x ---- x   <- next_loop_pair
+    # |      |
+    # o ---- o   <- pair
+    def get_next_loop_pair(pair):
+        lp = pair[0].link_loop_prev
+        if lp.vert == pair[1].vert:
+            lp = pair[0].link_loop_next
+            if lp.vert == pair[1].vert:
+                # no loop is found
+                return None
+
+        ln = pair[1].link_loop_next
+        if ln.vert == pair[0].vert:
+            ln = pair[1].link_loop_prev
+            if ln.vert == pair[0].vert:
+                # no loop is found
+                return None
+        return [lp, ln]
+
+    # % ---- %   <- next_poly_loop_pair
+    # |      |
+    # x ---- x   <- next_loop_pair
+    # |      |
+    # o ---- o   <- pair
+    def get_next_poly_loop_pair(pair):
+        v1 = pair[0].vert
+        v2 = pair[1].vert
+        for l1 in v1.link_loops:
+            for l2 in v2.link_loops:
+                if l1.link_loop_next == l2:
+                    return [l1, l2]
+        # no next poly loop is found
+        return None
+
+
+    def get_loop_sequence(pairs, island_info):
+        loop_seqs = {}
+        for pair in pairs:
+            seqs = [pair]
+            p = pair
+            isl_grp = get_island_group_by_pair(pair, island_info)
+            print(isl_grp)
+            if isl_grp == -1:
+                return None     # error
+
+            while True:
+                nlp = get_next_loop_pair(p)
+                if not nlp:
+                    break       # no more loop pair
+                nlp_isl_grp = get_island_group_by_pair(nlp, island_info)
+                if nlp_isl_grp != isl_grp:
+                    break       # another island
+
+                seqs.append(nlp)
+
+                nplp = get_next_poly_loop_pair(nlp)
+                if not nplp:
+                    break       # no more loop pair
+                nplp_isl_grp = get_island_group_by_pair(nplp, island_info)
+                if nplp_isl_grp != isl_grp:
+                    break       # another island
+                seqs.append(nplp)
+
+                p = nplp
+
+            for seq in seqs:
+                seq[0][uv_layer].select = True
+                seq[1][uv_layer].select = True
+            pprint(seqs)
+
+        #print(loop_seqs)
+
+
+    def get_island_group(loop, island_info):
+        for i, isl in enumerate(island_info):
+            for f in isl['faces']:
+                for l in f['face'].loops:
+                    if l == loop:
+                        return i
+        return -1
+
+
+    def get_island_group_by_pair(pair, island_info):
+        l1_grp = get_island_group(pair[0], island_info)
+        l2_grp = get_island_group(pair[1], island_info)
+
+        if (l1_grp == -1) or (l2_grp == -1) or (l1_grp != l2_grp):
+            return -1
+
+        return l1_grp
+
+
+    isl_info = muv_common.get_island_info_from_bmesh(bm, False)
+    get_loop_pairs(first_loop, loop_pairs, uv_layer, parsed_loops)
+    loop_pairs = sort_loop_pairs(loop_pairs)
+    get_loop_sequence(loop_pairs, isl_info)
+
+    #pprint(loop_pairs)
+    #pprint(next_loop_pairs)
+
+
+
+    return
+
+    # pick up first loop
+    #first_loop = cand_loops[0]
+    #ordered_loops = [first_loop]
+    for l in cand_loops:
+        if l[uv_layer].select and l.link_loop_next[uv_layer].select:
+            first_loop = l
+            break
+    else:
+        return
+
+    ordered_loop = [first_loop]
+    loop = first_loop
+    while True:
+
+
+        if not loop:
+            print("break")
+            break
+        loop_next = loop.link_loop_next
+        if loop[uv_layer].select and loop_next[uv_layer].select:
+            print("--------------")
+            print("PAIR")
+            print(loop)
+            print(loop_next)
+            print("OTHER")
+            next_vert = loop_next.vert
+            for l in next_vert.link_loops:
+                if loop_next != l:
+                    print(l)
+                    if l[uv_layer].select:
+                        loop = l
+                        break
+            else:
+                break
+            continue
+        loop_prev = loop.link_loop_prev
+        if loop[uv_layer].select and loop_prev[uv_layer].select:
+            prev
+        break
+
+    print("FIN")
+    return
+
+    # pick up first loop
+    vert_loop = []
+    loop = cand_loops[0]
+    vert = loop.vert
+    sel_loop = [l for l in vert.link_loops if l[uv_layer].select]
+    vert_loop.append({vert: sel_loop})
+    pprint(vert_loop)
 
 class MUV_AUVCircle(bpy.types.Operator):
 
@@ -74,6 +362,10 @@ class MUV_AUVCircle(bpy.types.Operator):
         obj = context.active_object
         bm = bmesh.from_edit_mesh(obj.data)
         uv_layer = bm.loops.layers.uv.verify()
+
+        get_vert_loop(bm, uv_layer)
+        bmesh.update_edit_mesh(obj.data)
+        return {'CANCELLED'}
 
         sel_faces = [f for f in bm.faces if f.select]
 
